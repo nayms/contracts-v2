00145a0d630602bccf461b18f24c9bade3cd6edd
Ramesh Nair <ram@hiddentao.com>
2019-12-19 18:30:16 +0000

    more tests

--- test/etherToken.js
+++ test/etherToken.js
@@ -4,6 +4,8 @@ import { events } from '../'
 import { ensureAclIsDeployed } from '../migrations/utils/acl'
 import { ensureEtherTokenIsDeployed } from '../migrations/utils/etherToken'
 
+const IERC20 = artifacts.require("./base/IERC20")
+
 const getBalance = async a => {
   const bal = await web3.eth.getBalance(a)
   return new EthVal(bal)
@@ -82,6 +84,121 @@ contract('EtherToken', accounts => {
     it('withdraw() fails if insufficent balance', async () => {
       await etherToken.deposit({ value: 23 })
       await etherToken.withdraw(24).should.be.rejectedWith('EtherToken: insufficient balance')
+    })
+  })
+
+  describe('supports ERC20 operations', () => {
+    let tkn
+    let tknSupply
+
+    beforeEach(async () => {
+      await etherToken.deposit({ value: 1000 })
+      tkn = await IERC20.at(etherToken.address)
+      tknSupply = await tkn.totalSupply()
+    })
+
+    describe('transfer authorization', async () => {
+      it('not just anyone can do a transfer', async () => {
+        await tkn.transfer(accounts[1], 10).should.be.rejectedWith('EtherToken: msg.sender is unauthorized')
+      })
+
+      it('and only admins can authorize transfer operators', async () => {
+        await etherToken.setAllowedTransferOperator(accounts[0], true, { from: accounts[1] }).should.be.rejectedWith('must be admin')
+        await etherToken.setAllowedTransferOperator(accounts[0], true, { from: accounts[0] }).should.be.fulfilled
+      })
+
+      it('people can be authorized to do transfers', async () => {
+        await etherToken.setAllowedTransferOperator(accounts[0], true)
+        await tkn.transfer(accounts[0], 10).should.be.fulfilled
+        await etherToken.setAllowedTransferOperator(accounts[0], false)
+        await tkn.transfer(accounts[1], 10).should.be.rejectedWith('EtherToken: msg.sender is unauthorized')
+      })
+
+      it('and this applies to transferFrom() too', async () => {
+        await etherToken.approve(accounts[1], 200).should.be.fulfilled
+
+        await etherToken.setAllowedTransferOperator(accounts[1], false)
+
+        await tkn.transferFrom(accounts[0], accounts[1], 10, { from: accounts[1] }).should.be.rejectedWith('EtherToken: msg.sender is unauthorized')
+
+        await etherToken.setAllowedTransferOperator(accounts[1], true)
+
+        await tkn.transferFrom(accounts[0], accounts[1], 10, { from: accounts[1] }).should.be.fulfilled
+      })
+    })
+
+    it('such as approving an address to send on one\'s behalf', async () => {
+      const result = await tkn.approve(accounts[1], 2).should.be.fulfilled
+
+      expect(extractEventArgs(result, events.Approval)).to.include({
+        owner: accounts[0],
+        spender: accounts[1],
+        value: '2',
+      })
+
+      await tkn.allowance(accounts[0], accounts[1]).should.eventually.eq(2)
+    })
+
+    describe('such as transferring one\'s own tokens', () => {
+      beforeEach(async () => {
+        await etherToken.setAllowedTransferOperator(accounts[0], true, { from: accounts[0] }).should.be.fulfilled
+      })
+
+      it('but not when sender does not have enough', async () => {
+        await tkn.transfer(accounts[1], tknSupply + 1).should.be.rejectedWith('EtherToken: transfer amount exceeds balance')
+      })
+
+      it('the entire balance of a user if need be', async () => {
+        await tkn.transfer(accounts[1], tknSupply).should.be.fulfilled
+
+        await tkn.balanceOf(accounts[0]).should.eventually.eq(0)
+        await tkn.balanceOf(accounts[1]).should.eventually.eq(tknSupply)
+      })
+
+      it('when the sender has enough', async () => {
+        const result = await tkn.transfer(accounts[1], 5).should.be.fulfilled
+
+        await tkn.balanceOf(accounts[0]).should.eventually.eq(tknSupply - 5)
+        await tkn.balanceOf(accounts[1]).should.eventually.eq(5)
+
+        expect(extractEventArgs(result, events.Transfer)).to.include({
+          from: accounts[0],
+          to: accounts[1],
+          value: '5',
+        })
+      })
+    })
+
+    describe('such as transferring another person\'s tokens', () => {
+      beforeEach(async () => {
+        await etherToken.setAllowedTransferOperator(accounts[0], true, { from: accounts[0] }).should.be.fulfilled
+        await etherToken.setAllowedTransferOperator(accounts[1], true, { from: accounts[0] }).should.be.fulfilled
+      })
+
+      it('but not if sender is not approved', async () => {
+        await tkn.transferFrom(accounts[0], accounts[2], 5, { from: accounts[0] }).should.be.rejectedWith('EtherToken: transfer amount exceeds allowance')
+      })
+
+      it('but not if sender exceeds their approved limit', async () => {
+        await tkn.approve(accounts[1], 2).should.be.fulfilled
+        await tkn.transferFrom(accounts[0], accounts[2], 5, { from: accounts[1] }).should.be.rejectedWith('EtherToken: transfer amount exceeds allowance')
+      })
+
+      it('if sender meets their approved limit', async () => {
+        await tkn.approve(accounts[1], 5).should.be.fulfilled
+
+        const result =
+          await tkn.transferFrom(accounts[0], accounts[2], 5, { from: accounts[1] }).should.be.fulfilled
+
+        await tkn.balanceOf(accounts[0]).should.eventually.eq(tknSupply - 5)
+        await tkn.balanceOf(accounts[2]).should.eventually.eq(5)
+
+        expect(extractEventArgs(result, events.Transfer)).to.include({
+          from: accounts[0],
+          to: accounts[2],
+          value: '5',
+        })
+      })
     })
   })
 })

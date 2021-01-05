f98ae5cdcc3bfe329dcf18b505a2adbe501148db
Ramesh Nair <ram@hiddentao.com>
2021-01-05 15:55:04 +0000

    fix token names and symbols

---
+++ contracts/base/Strings.sol
@@ -0,0 +1,64 @@
+pragma solidity >=0.6.7;
+
+/**
+ * Strings Library
+ * 
+ * In summary this is a simple library of string functions which make simple 
+ * string operations less tedious in solidity.
+ * 
+ * Please be aware these functions can be quite gas heavy so use them only when
+ * necessary not to clog the blockchain with expensive transactions.
+ * 
+ * @author James Lockhart <james@n3tw0rk.co.uk>
+ */
+library Strings {
+    /**
+     * Sub String
+     * 
+     * Extracts the beginning part of a string based on the desired length
+     * 
+     * @param _base When being used for a data type this is the extended object
+     *              otherwise this is the string that will be used for 
+     *              extracting the sub string from
+     * @param _length The length of the sub string to be extracted from the base
+     * @return string The extracted sub string
+     */
+    function substring(string memory _base, int _length)
+        internal
+        pure
+        returns (string memory) {
+        return _substring(_base, _length, 0);
+    }
+
+    /**
+     * Sub String
+     * 
+     * Extracts the part of a string based on the desired length and offset. The
+     * offset and length must not exceed the lenth of the base string.
+     * 
+     * @param _base When being used for a data type this is the extended object
+     *              otherwise this is the string that will be used for 
+     *              extracting the sub string from
+     * @param _length The length of the sub string to be extracted from the base
+     * @param _offset The starting point to extract the sub string from
+     * @return string The extracted sub string
+     */
+    function _substring(string memory _base, int _length, int _offset)
+        internal
+        pure
+        returns (string memory) {
+        bytes memory _baseBytes = bytes(_base);
+
+        assert(uint(_offset + _length) <= _baseBytes.length);
+
+        string memory _tmp = new string(uint(_length));
+        bytes memory _tmpBytes = bytes(_tmp);
+
+        uint j = 0;
+        for (uint i = uint(_offset); i < uint(_offset + _length); i++) {
+            _tmpBytes[j++] = _baseBytes[i];
+        }
+
+        return string(_tmpBytes);
+    }
+}

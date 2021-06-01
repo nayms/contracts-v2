439065bc24e7803c53cfb5d6031072ab36217e6b
Ramesh Nair <ram@hiddentao.com>
2021-06-01 13:45:19 +0100

    CU-kdadza: tests updated

--- contracts/PolicyFacetBase.sol
+++ contracts/PolicyFacetBase.sol
@@ -69,4 +69,10 @@ abstract contract PolicyFacetBase is EternalStorage, IPolicyStates, AccessContro
       return 0;
     }
   }
+
+  function _tranchPaymentsAllMade (uint256 _index) internal view returns (bool) {
+    uint256 numPremiums = dataUint256[__i(_index, "numPremiums")];
+    uint256 numPremiumsPaid = dataUint256[__i(_index, "numPremiumsPaid")];
+    return (numPremiumsPaid == numPremiums);
+  }
 }

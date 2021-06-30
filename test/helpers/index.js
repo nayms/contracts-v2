0868e013f27fb9b6465c28595a2e9ac7869802ec
Nemitari Ajienka <nemitariajienka@Nemitaris-iMac.local>
2021-06-30 10:46:11 +0100

    CU-mz87nx: removing added custom test helpers

--- test/helpers/index.js
+++
@@ -1,8 +0,0 @@
-const {expect} = require('chai');
-
-const expectBignumberEqual = (a, b) => expect(a.toString()).to.be.equal(b.toString());
-
-module.exports = {
-  expect,
-  expectBignumberEqual
-};

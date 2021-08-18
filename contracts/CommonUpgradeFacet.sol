f03d852c4f0857254cbe0111e14d5deb6936f20b
Nayms Engineering <engineering@nayms.io>
2021-08-18 09:45:01 +0000

    Release 1.0.0-pr.117.build.1321

--- contracts/CommonUpgradeFacet.sol
+++ contracts/CommonUpgradeFacet.sol
@@ -14,8 +14,8 @@ contract CommonUpgradeFacet is Controller, IDiamondUpgradeFacet {
   }
 
   function getVersionInfo () public override pure returns (string memory num_, uint256 date_, string memory hash_) {
-    num_ = "1.0.0-local.1626957185098";
-    date_ = 1626957185;
-    hash_ = "43420b279b42a2828bb9e432bbcedce6850cbecd";
+    num_ = "1.0.0-pr.117.build.1321";
+    date_ = 1629279174;
+    hash_ = "82804ef5eed94e39897a4c028fc0258d383ee820";
   }
 }

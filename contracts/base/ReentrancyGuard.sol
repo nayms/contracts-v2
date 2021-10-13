bfa33e8bf733d11289a8c6abce96be683606f69d
Ramesh Nair <ram@hiddentao.com>
2021-10-13 13:55:52 +0100

    add SPDX license identifer

--- contracts/base/ReentrancyGuard.sol
+++ contracts/base/ReentrancyGuard.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
 // From OpenZeppellin: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol

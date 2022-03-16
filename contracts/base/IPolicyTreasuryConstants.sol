a4f761bfed203cf8be416a4304cd5ca25752fea8
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2022-03-16 15:41:42 +0100

    drop solidity version down to 0.8.9

--- contracts/base/IPolicyTreasuryConstants.sol
+++ contracts/base/IPolicyTreasuryConstants.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity 0.8.9;
 
 abstract contract IPolicyTreasuryConstants {
   bytes32 constant public ORDER_TYPE_TOKEN_SALE = 0xfca7e79cb5091f353eb60204c9aee8a98531c6069472e235576044613bd73961;
--- scripts/setupReleaseConfig.js
+++ scripts/setupReleaseConfig.js
@@ -73,7 +73,7 @@ async function main () {
 
   // update solidity contract
   fs.writeFileSync(commonUpgradeFacetContract, `// SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity 0.8.9;
 
 import "./base/Controller.sol";
 import "./base/IDiamondUpgradeFacet.sol";

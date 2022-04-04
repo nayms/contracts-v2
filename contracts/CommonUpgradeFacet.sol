775e0dda1d0a5f607930c1a01e8ccb4e720f6deb
Nayms Engineering <engineering@nayms.io>
2022-04-04 16:51:06 +0000

    Release 1.0.0-build.1748

--- contracts/CommonUpgradeFacet.sol
+++ contracts/CommonUpgradeFacet.sol
@@ -6,26 +6,17 @@ import "./base/IDiamondUpgradeFacet.sol";
 import "./base/IDiamondProxy.sol";
 
 contract CommonUpgradeFacet is Controller, IDiamondUpgradeFacet {
-    constructor(address _settings) Controller(_settings) {
-        // empty
-    }
+  constructor (address _settings) Controller(_settings) public {
+    // empty
+  }
 
-    function upgrade(address[] memory _facets) public override assertIsAdmin {
-        IDiamondProxy(address(this)).registerFacets(_facets);
-    }
+  function upgrade (address[] memory _facets) public override assertIsAdmin {
+    IDiamondProxy(address(this)).registerFacets(_facets);
+  }
 
-    function getVersionInfo()
-        public
-        pure
-        override
-        returns (
-            string memory num_,
-            uint256 date_,
-            string memory hash_
-        )
-    {
-        num_ = "1.0.0-build.dev1648881199382";
-        date_ = 1648881199;
-        hash_ = "7a600d5c0d84633ddce6dd03877d02979446fabe";
-    }
+  function getVersionInfo () public override pure returns (string memory num_, uint256 date_, string memory hash_) {
+    num_ = "1.0.0-build.1748";
+    date_ = 1649088980;
+    hash_ = "ea5dfbc0436e5577e5c77d3a58d66124eabd08da";
+  }
 }

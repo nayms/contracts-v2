d0ed548da5d9a50078d445a2f8fee49c085c2d45
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2022-05-12 09:46:57 +0200

    comment out unused util methods, to regain coverage

--- contracts/base/Strings.sol
+++ contracts/base/Strings.sol
@@ -108,34 +108,34 @@ library Strings {
         return string(buffer);
     }
 
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
-     */
-    function toHexString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) {
-            return "0x00";
-        }
-        uint256 temp = value;
-        uint256 length = 0;
-        while (temp != 0) {
-            length++;
-            temp >>= 8;
-        }
-        return toHexString(value, length);
-    }
+    // /**
+    //  * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
+    //  */
+    // function toHexString(uint256 value) internal pure returns (string memory) {
+    //     if (value == 0) {
+    //         return "0x00";
+    //     }
+    //     uint256 temp = value;
+    //     uint256 length = 0;
+    //     while (temp != 0) {
+    //         length++;
+    //         temp >>= 8;
+    //     }
+    //     return toHexString(value, length);
+    // }
 
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
-     */
-    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
-        bytes memory buffer = new bytes(2 * length + 2);
-        buffer[0] = "0";
-        buffer[1] = "x";
-        for (uint256 i = 2 * length + 1; i > 1; --i) {
-            buffer[i] = _HEX_SYMBOLS[value & 0xf];
-            value >>= 4;
-        }
-        require(value == 0, "Strings: hex length insufficient");
-        return string(buffer);
-    }
+    // /**
+    //  * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+    //  */
+    // function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
+    //     bytes memory buffer = new bytes(2 * length + 2);
+    //     buffer[0] = "0";
+    //     buffer[1] = "x";
+    //     for (uint256 i = 2 * length + 1; i > 1; --i) {
+    //         buffer[i] = _HEX_SYMBOLS[value & 0xf];
+    //         value >>= 4;
+    //     }
+    //     require(value == 0, "Strings: hex length insufficient");
+    //     return string(buffer);
+    // }
 }
